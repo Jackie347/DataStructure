5c051a83d5ef91d42d0d5a4f01e056d35370c088
Jackie347 <43152813+Jackie347@users.noreply.github.com>
2020-11-03 23:54:15 +0800

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -11,30 +11,30 @@ public class SinglyLinkedList<E> {
 		private Node<E> next;
 		
 		/**
-         * Construct a node with the given data value and link
-         * @param data - The data value
-         * @param next - The link
-         */
+         	* Construct a node with the given data value and link
+        	 * @param data - The data value
+         	* @param next - The link
+         	*/
 		public Node(E data, Node<E> next) {
 			this.data = data;
 			this.next = next;
 		}
 		
 		/**
-         * Construct a node with only the given data value
-         * @param data - The data value
-         */
-        public Node(E data) {
-            this(data, null);
-        }
-        
-        /**
-         * Construct a head node
-         * @param next - The very first node of list
-         */
-        public Node(Node<E> next) {
-            this(null, next);
-        }
+		 * Construct a node with only the given data value
+		 * @param data - The data value
+		 */
+		public Node(E data) {
+		    this(data, null);
+		}
+
+		/**
+		 * Construct a head node
+		 * @param next - The very first node of list
+		 */
+		public Node(Node<E> next) {
+		    this(null, next);
+		}
 	}
 	
 	
@@ -47,10 +47,10 @@ public class SinglyLinkedList<E> {
 	// Helper Methods
 	
 	/**
-     * Find the node at a specified index
-     * @param index - The index of the wanted node
-     * @returns The node at index or null if it does not exist
-     */
+     	* Find the node at a specified index
+     	* @param index - The index of the wanted node
+    	* @returns The node at index or null if it does not exist
+    	*/
 	private Node<E> getNode(int index){
 		Node<E> p = head;
 		for (int i = -1; i < index && p != null; i++) {
@@ -63,19 +63,19 @@ public class SinglyLinkedList<E> {
 	// Public Methods
 	
 	/**
-     * Get the size of the list
-     * @return The number of nodes in the list
-     */
-    int size() {
-        return size;
-    }
+     	* Get the size of the list
+     	* @return The number of nodes in the list
+     	*/
+    	int size() {
+       		return size;
+    	}
 	
 	/**
-     * Get the data value at index
-     * @param index - The index of wanted node
-     * @returns The data of node at index
-     * @throws IndexOutOfBoundsException if the index is out of range
-     */
+     	* Get the data value at index
+     	* @param index - The index of wanted node
+     	* @returns The data of node at index
+     	* @throws IndexOutOfBoundsException if the index is out of range
+     	*/
 	public E get(int index) {
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException(Integer.toString(index));
@@ -85,11 +85,11 @@ public class SinglyLinkedList<E> {
 	}
 	
 	/**
-     * Set the data value at index
-     * @param index - The index of the node to change
-     * @param value - The new value
-     * @throws IndexOutOfBoundsException if the index is out of range
-     */
+     	* Set the data value at index
+     	* @param index - The index of the node to change
+     	* @param value - The new value
+     	* @throws IndexOutOfBoundsException if the index is out of range
+     	*/
 	public void set(int index, E value) {
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException(Integer.toString(index));
@@ -99,11 +99,11 @@ public class SinglyLinkedList<E> {
 	}
 	
 	/**
-     * Insert a node at the specified position in the list.
-     * @param index - Index at which the specified item is to be inserted
-     * @param value - The value of node to be inserted
-     * @throws IndexOutOfBoundsException if the index is out of range
-     */
+     	* Insert a node at the specified position in the list.
+     	* @param index - Index at which the specified item is to be inserted
+     	* @param value - The value of node to be inserted
+     	* @throws IndexOutOfBoundsException if the index is out of range
+     	*/
 	public void insert(int index, E value) {
 		if (index < 0 || index > size) {
 			throw new IndexOutOfBoundsException(Integer.toString(index));
@@ -121,18 +121,18 @@ public class SinglyLinkedList<E> {
 	}
 	
 	/**
-     * Append the specified node to the end of the list
-     * @param value - The value of node to be appended
-     */
+     	* Append the specified node to the end of the list
+     	* @param value - The value of node to be appended
+    	*/
 	public void append(E value) {
 		insert(size, value);
 	}
 		
 	/**
-     * Remove the node at the specified position in the list. 
-     * @param index - The index of the node to be removed
-     * @throws IndexOutOfBoundsException if the index is out of range
-     */
+     	* Remove the node at the specified position in the list. 
+     	* @param index - The index of the node to be removed
+     	* @throws IndexOutOfBoundsException if the index is out of range
+     	*/
 	public void remove(int index) {
 		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException(Integer.toString(index));
@@ -150,26 +150,24 @@ public class SinglyLinkedList<E> {
 	}
 	
 	/**
-     * Obtain a string representation of the list to help printing
-     * @return A String representation of the list
-     */
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder("[");
-        Node<E> p = head;
-        if (p != null) {
-            while (p.next != null) {
-            	p = p.next;
-                sb.append(p.data.toString());
-                if (p.next != null) {
-                	sb.append(" ==> ");
-                }               
-            }
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-
-	
+     	* Obtain a string representation of the list to help printing
+     	* @return A String representation of the list
+     	*/
+    	@Override
+    	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		Node<E> p = head;
+		if (p != null) {
+		    while (p.next != null) {
+			p = p.next;
+			sb.append(p.data.toString());
+			if (p.next != null) {
+				sb.append(" ==> ");
+			}               
+		    }
+		}
+		sb.append("]");
+		return sb.toString();
+    	}	
 	
 }
